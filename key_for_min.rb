e107ace1865be3771a02ff9488070ee4da659a70
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-03-01 02:10:30 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.length > 0
+    answerKey = name_hash.first[0]
+    smallestValue = name_hash.first[1]
+    name_hash.each do |key , value|
+      if value < smallestValue
+        answerKey = key
+        smallestValue = value
+      end
+    end
+    return answerKey
+  end
 end
