e338326441f18231363d8d6630a53370ab064867
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-03-01 01:54:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  num = name_hash.keys[0]
+  num = name_hash.first
   return num
   # name_hash.collect do |key, value|
   #   return num
