d984b8c327db8606f9564779268b0362037fee36
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-03-01 01:59:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
   if name_hash.length > 0
     num = name_hash.first.keys
   end
-
-  num = name_hash.first
-  return num
+  # 
+  # num = name_hash.first
+  # return num
   # name_hash.each_value do |value|
   #   return num
   #
